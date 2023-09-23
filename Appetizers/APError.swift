ebe0096d6df0dbd33f9d2e9fd6efb70e94ea3a86
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-09-23 02:05:55 -0300

    feat: add error enum and handling errors in view model

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Cristóbal Contreras on 23-09-23.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
---
+++ Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  AppetizerListViewModel.swift
+//  Appetizers
+//
+//  Created by Cristóbal Contreras on 23-09-23.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    @Published var appetizers: Appetizers = []
+    @Published var alertItem: AlertItem?
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    switch error {
+                    case .invalidResponse:
+                        self.alertItem = AlertContext.invalidResponse
+                    case .invalidURL:
+                        self.alertItem = AlertContext.invalidURL
+                    case .invalidData:
+                        self.alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        self.alertItem = AlertContext.unableToComplete
+                    }
+                }
+            }
+        }
+    }
+}
