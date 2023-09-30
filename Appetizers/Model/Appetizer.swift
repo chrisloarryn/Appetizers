0d433d0f095f7522810fa018d050d517c6991815
Cristobal Andres Contreras Orellana <ccontreraso@pedrodevaldivia.cl>
2023-09-29 21:03:52 -0300

    feat: add order screen

--- Appetizers/Model/Appetizer.swift
+++ Appetizers/Model/Appetizer.swift
@@ -38,4 +38,33 @@ struct MockData {
     static let appetizers: Appetizers = [sampleAppetizer,sampleAppetizer,sampleAppetizer,sampleAppetizer]
     
     static let appetizerResponse: AppetizerResponse = AppetizerResponse(request: appetizers)
+    
+    static let orderItemOne     = Appetizer(id: 0001,
+                                              name: "Test Appetizer One",
+                                              description: "This is the description for my appetizer. It's yummy.",
+                                              price: 9.99,
+                                              imageURL: "",
+                                            carbs: 99,
+                                            protein: 99,
+                                            calories: 99)
+       
+    static let orderItemTwo     = Appetizer(id: 0002,
+                                            name: "Test Appetizer Two",
+                                            description: "This is the description for my appetizer. It's yummy.",
+                                            price: 9.99,
+                                            imageURL: "",
+                                            carbs: 99,
+                                            protein: 99,
+                                            calories: 99)
+       
+    static let orderItemThree   = Appetizer(id: 0003,
+                                            name: "Test Appetizer Three",
+                                            description: "This is the description for my appetizer. It's yummy.",
+                                            price: 9.99,
+                                            imageURL: "",
+                                            carbs: 99,
+                                            protein: 99,
+                                            calories: 99)
+       
+    static let orderItems       = [orderItemOne, orderItemTwo, orderItemThree]
 }
